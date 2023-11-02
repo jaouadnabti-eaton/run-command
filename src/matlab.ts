51ed3bdb3d36cf849342cf91a89f9d1d1e9bce59
David Buzinski <davidb@mathworks.com>
2023-11-02 09:39:28 -0400

    trying something dumb

--- src/matlab.ts
+++ src/matlab.ts
@@ -62,7 +62,8 @@ export async function runCommand(hs: HelperScript, platform: string, architectur
        execArgs = execArgs.concat(args);
     }
 
-    const exitCode = await fn(rmcPath, execArgs);
+    const exitCode = await fn(rmcPath, ["ver"]);
+    // const exitCode = await fn(rmcPath, execArgs);
     if (exitCode !== 0) {
         return Promise.reject(Error(`Exited with non-zero code ${exitCode}`));
     }
